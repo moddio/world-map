190ab7988cfceb2db450892138c9e865ad63e9ae
MERNinja <shailesh@modd.io>
2024-04-12 19:52:20 +0530

    Logo UI

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  // Route,
+  Route,
   Routes /*useNavigate*/,
 } from 'react-router-dom';
 // import Navbar from "./Navbar";
 // import Login from './Login';
-// import RegisterModal from './RegisterModal';
+import RegisterModal from './RegisterModal';
 import MapComponent from './map';
-// import Auth from './Auth';
-// import Navbar from './Navbar';
+import Auth from './Auth';
 
 const Layout = () => {
   // const userDetails = JSON.parse(localStorage.getItem('user'));
@@ -19,14 +18,14 @@ const Layout = () => {
     <Router>
       <div className='bg-white' style={{ backgroundColor: '#f0f8f0' }}>
         {/* <Navbar /> */}
-        <h1 className='bg-[#1883FD] mb-0 text-white text-left'>
+        <h1 className='fixed max-md:text-xl lg:text-5xl font-bold text-white text-left p-2 my-0'>
           Doomr.io
         </h1>
         <MapComponent />
         <Routes>
           {/* <Route path='/' element={<Home />} />*/}
-          {/* <Route path='/login' element={<Auth />} />
-          <Route path='/signup' element={<RegisterModal />} /> */}
+          <Route path='/login' element={<Auth />} />
+          <Route path='/signup' element={<RegisterModal />} />
         </Routes>
       </div>
     </Router>
