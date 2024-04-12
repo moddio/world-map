d40b80900af2b9e9c1fb8737a905c742bf62d640
MERNinja <shailesh@modd.io>
2024-04-12 12:55:45 +0530

    Changed the game json data

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const siteUrl = 'https://www.modd.io'
-export const worldMapId = '66041cba0fc564c882603662'
+export const siteUrl = 'https://www.modd.io';
+export const worldMapId = '65ae9e9be30e53ca0e8f70e7'; // LAD Game ID
