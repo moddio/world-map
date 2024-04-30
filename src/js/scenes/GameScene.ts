d2c4f22b1e37dcbabcdebf676af6bf781ff2e651
MERNinja <155755504+MERNinja@users.noreply.github.com>
2024-04-30 19:17:20 +0530

    Update GameScene.ts

--- src/js/scenes/GameScene.ts
+++ src/js/scenes/GameScene.ts
@@ -160,7 +160,7 @@ export default class GameScene extends Phaser.Scene {
 
     // Create the outer div element
     const outerDiv = document.createElement("div");
-    outerDiv.className="bg-[#5d509b] p-1 rounded-lg z-0 opacity-100"
+    outerDiv.className="bg-[#5d509b] px-1 py-0 rounded-lg bg-opacity-85"
     outerDiv.setAttribute("id", `marker-${tileInfo.id}`);
 
     // Create the inner div element
@@ -169,7 +169,7 @@ export default class GameScene extends Phaser.Scene {
       "style",
       "color: white; font: 11px Arial; display:flex;"
     );
-    innerDiv.innerHTML = ` <div style="margin-right: 2px;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3 h-3">
+    innerDiv.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-[2px] h-3">
     <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z"></path>
     </svg></div>
     <span>${tileInfo.totalActivePlayers}</span>`;
@@ -180,7 +180,7 @@ export default class GameScene extends Phaser.Scene {
 
     this.add.dom(
       (tileInfo.position.x * this.tileSize) + 10,
-      (tileInfo.position.y * this.tileSize - this.tileSize / 2) + 5,
+      (tileInfo.position.y * this.tileSize - this.tileSize / 2) + 12,
       outerDiv
     );
   }
